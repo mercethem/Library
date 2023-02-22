4261f5321c3166d473b64cefc797e866529f7742
Ethem MERÇ <114696279+mercethem@users.noreply.github.com>
2023-02-22 18:17:14 +0300

    Classes and functions was created

---
+++ src/Menu.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Menu {
+    static void menu(){
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Please enter choose a option from downside : ");
+        byte options = keyboard.nextByte();
+        switch (options){
+            case 0: DisplayOptions.displayOptions();
+            case 1: Add.add();
+            case 2: Delete.delete();
+            case 3: Borrow.borrow();
+            case 4: Returning.returning();
+        }
+    }
+
+}
