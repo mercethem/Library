bb5481985abc8d27292fb6eff7bee0d99ad3fd80
Ethem MERÇ <114696279+mercethem@users.noreply.github.com>
2023-02-28 17:30:15 +0300

    Exceptions deleted

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 import java.sql.SQLException;
 public class Main {
-    public static void main(String[] args) throws SQLException, ClassNotFoundException, InstantiationException, IllegalAccessException {
+    public static void main(String[] args) {
         Menu.menu();
     }
 }
