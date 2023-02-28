c9cb84f9f81e6646b54496a2dc0aba283b0c81f6
Ethem MERÇ <114696279+mercethem@users.noreply.github.com>
2023-02-28 17:28:19 +0300

    reformat

--- src/DataBaseLayer.java
+++ src/DataBaseLayer.java
@@ -1,14 +1,15 @@
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
 
 public class DataBaseLayer {
     static Connection myConnection;
     static String url = "jdbc:sqlserver://DESKTOP-T9F59R5\\MSSQLSERVER:1433;useSSL=false;encrypt=false;integratedSecurity=true;"; // table details
+
     public static Connection dataBaseLayer() {
-        try{
-        Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver"); // Driver name
-        myConnection = DriverManager.getConnection(url);
-    }
-        catch (Exception e){
+        try {
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver"); // Driver name
+            myConnection = DriverManager.getConnection(url);
+        } catch (Exception e) {
             System.out.println("Connection is failed!");
         }
         return myConnection;
