3b1d922e7f22178bd344181a8e2ec16b58b97b97
Ethem MERÇ <114696279+mercethem@users.noreply.github.com>
2023-02-28 17:30:29 +0300

    Exceptions deleted and new controls added

--- src/Menu.java
+++ src/Menu.java
@@ -1,18 +1,17 @@
-import java.sql.SQLException;
 import java.util.Scanner;
 public class Menu {
-     static void menu() throws SQLException, ClassNotFoundException, InstantiationException, IllegalAccessException {
+     static void menu() {
         Scanner keyboard = new Scanner(System.in);
-        System.out.println("Please enter choose a option from downside : ");
+        System.out.println("Please enter a choose from options downside : ");
         byte options = keyboard.nextByte();
         switch (options){
             case 0: DisplayOptions.displayOptions();
-            case 1: Add.add();
+            case 1: Add.addBook();
             case 2: Delete.delete();
             case 3: Borrow.borrow();
             case 4: Returning.returning();
             case 5: DataBaseLayer.dataBaseLayer();
-            case 6: DisplayBook.displayOptionsWithAuthorname();  //TODO Temporary!!! When finished delete this case
+            case 6: Add.addBook();  //TODO Temporary!!! When finished delete this case
         }
     }
 
