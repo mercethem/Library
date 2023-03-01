2e3b3cc6edaded539638c6202c418c923a83fa63
Ethem MERÇ <114696279+mercethem@users.noreply.github.com>
2023-03-02 01:03:19 +0300

    Added comments

--- src/DataBaseLayer.java
+++ src/DataBaseLayer.java
@@ -3,9 +3,9 @@ import java.sql.DriverManager;
 
 public class DataBaseLayer {
     static Connection myConnection;
-    static String url = "jdbc:sqlserver://DESKTOP-T9F59R5\\MSSQLSERVER:1433;useSSL=false;encrypt=false;integratedSecurity=true;"; // table details
+    static String url = "jdbc:sqlserver://DESKTOP-T9F59R5\\MSSQLSERVER:1433;useSSL=false;encrypt=false;integratedSecurity=true;"; // Database URL
 
-    public static Connection dataBaseLayer() {
+    public static Connection dataBaseLayer() { //Database layer function
         try {
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver"); // Driver name
             myConnection = DriverManager.getConnection(url);
