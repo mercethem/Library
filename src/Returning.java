7d64e468fed09414969990fdf21ef2ce866c8204
Ethem MERÇ <114696279+mercethem@users.noreply.github.com>
2023-03-03 15:12:09 +0300

    All functions created and fixed

--- src/Returning.java
+++ src/Returning.java
@@ -1,5 +1,30 @@
+import java.sql.Statement;
+import java.util.Scanner;
+
 public class Returning {//Want to return book ?
-    static void returning(){
+
+    static void returning() {
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Please enter ID number of Student : ");
+        long studentId = keyboard.nextLong();
+        System.out.println("Please enter ID number of Book : ");
+        long bookId = keyboard.nextLong();
+        System.out.println("Please enter want to borrow ID Number of borrow : ");
+        long borrowId = keyboard.nextLong();
+        keyboard.nextLine();
+        try {//Database layer function and SQL query
+            DataBaseLayer.dataBaseLayer(); //Database layer function and SQL query
+            Statement myStatement = DataBaseLayer.myConnection.createStatement();
+            myStatement.executeQuery
+                    ("DELETE FROM LibraryStock.dbo.borrows WHERE studentId = '"+studentId+"' AND borrowId = '" + borrowId + "'"+
+                            "UPDATE LibraryStock.dbo.books SET amount =  amount + '1' WHERE bookId = '"+bookId+"'");
+            myStatement.close(); // close statement
+            DataBaseLayer.myConnection.close(); // close connection
+            Menu.menu();
+        } catch (Exception e) {
+            System.out.println();
+            Menu.menu();
+        }
 
     }
 }
