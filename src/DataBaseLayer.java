4908fae1115b452f081c79c7bb24c6815de342da
Ethem MERÇ <114696279+mercethem@users.noreply.github.com>
2023-03-03 15:11:21 +0300

    SQL Queries, Comments, TODOs and warnings fixed

--- src/DataBaseLayer.java
+++ src/DataBaseLayer.java
@@ -3,15 +3,15 @@ import java.sql.DriverManager;
 
 public class DataBaseLayer {
     static Connection myConnection;
-    static String url = "jdbc:sqlserver://DESKTOP-T9F59R5\\MSSQLSERVER:1433;useSSL=false;encrypt=false;integratedSecurity=true;"; // Database URL
+    static String url = "jdbc:sqlserver://DESKTOP-T9F59R5\\MSSQL SERVER:1433;useSSL=false;encrypt=false;integratedSecurity=true;"; // Database URL
 
-    public static Connection dataBaseLayer() { //Database layer function
+    public static void dataBaseLayer() { //Database layer function
         try {
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver"); // Driver name
             myConnection = DriverManager.getConnection(url);
+            System.out.println("Connections is successful!");
         } catch (Exception e) {
             System.out.println("Connection is failed!");
         }
-        return myConnection;
     }
 }
