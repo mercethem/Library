01ecba53a35e598d8bfa2d116bb1e87a6d1792f6
Ethem MERÇ <114696279+mercethem@users.noreply.github.com>
2023-03-03 15:11:02 +0300

    All functions created SQL Queries, Comments, TODOs and warnings fixed

--- src/Borrow.java
+++ src/Borrow.java
@@ -1,16 +1,35 @@
+import java.sql.Statement;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
 
 public class Borrow {
-    static void borrow(){
+    static void borrow() {
+        //Delete a student
         Scanner keyboard = new Scanner(System.in);
-        System.out.println("Please enter your Student ID number : ");
-        Long StudentID = keyboard.nextLong();
-        System.out.println("Please enter want to borrow book's name : ");
-        String bookName = keyboard.nextLine();
-        keyboard.next();
-
-
-
-
+        System.out.println("Please enter ID number of Student : ");
+        long studentId = keyboard.nextLong();
+        System.out.println("Please enter want to borrow ID Number of Book : ");
+        long bookId = keyboard.nextLong();
+        keyboard.nextLine();
+        DateTimeFormatter currentDate = DateTimeFormatter.ofPattern("dd/MM/yyyy"); //Brought date
+        LocalDateTime now = LocalDateTime.now();
+        LocalDateTime later = LocalDateTime.from(now).plusDays(30); //Return date
+        try {//Database layer function and SQL query
+            DataBaseLayer.dataBaseLayer(); //Database layer function and SQL query
+            Statement myStatement = DataBaseLayer.myConnection.createStatement();
+            myStatement.executeQuery
+                    ("INSERT INTO LibraryStock.dbo.borrows (studentId,bookId,takenDate,broughtDate ) " +
+                            "VALUES ('" + studentId + "', '" + bookId + "','" + currentDate.format(now) + "','"+currentDate.format(later)+"')"+
+                            "UPDATE LibraryStock.dbo.books SET amount =  amount - '1' WHERE bookId = '"+bookId+"'");
+            myStatement.close(); // close statement
+            DataBaseLayer.myConnection.close(); // close connection
+            Menu.menu();
+        } catch (Exception e) {
+            System.out.println();
+            Menu.menu();
+        }
     }
 }
+
+
